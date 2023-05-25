45146a019f598b23dafd5f72ecb26b69daeffdf2
AliciaGutierrezMartin <107947526+AliciaGutierrezMartin@users.noreply.github.com>
2023-05-25 19:24:10 +0200

    Add files via upload

--- proyecto_final/script.js
+++ proyecto_final/script.js
@@ -1,17 +1,22 @@
-
+// coge los ajustes del formulario y los guarda en sessionStorage o localStorage
 function guardarAjustes() {
     const ajustesNombre = document.getElementById('ajustesNombre').value;
     const ajustesModo = document.querySelector('input[name="modo"]:checked').value;
-    window.localStorage.setItem("--usuario", ajustesNombre);
+    const ajustesColor = document.getElementById('ajustesColor').value;
+
+    window.sessionStorage.setItem("--usuario", ajustesNombre);
     window.localStorage.setItem("--modo", ajustesModo);
+    window.localStorage.setItem("--colorMenu", ajustesColor);
     console.log("Ajustes actualizados");
     activarAjustes();
 }
 
 activarAjustes();
 
+// activar los ajustes que se han puesto en la página de ajustes
 function activarAjustes() {
-    let usuario = window.localStorage.getItem("--usuario");
+    // coge el usuario del sessionStorage y si existe lo pone en pantalla
+    let usuario = window.sessionStorage.getItem("--usuario");
     if (usuario) {
         document.getElementById('usuario').innerHTML = "Hola " + usuario + "!";
         const ajustesNombre = document.getElementById('ajustesNombre');
@@ -20,6 +25,18 @@ function activarAjustes() {
         }
     }
 
+    // coge el color del menú del localStorage y si existe lo pone de color en el menú
+    let colorMenu = window.localStorage.getItem("--colorMenu");
+    if(colorMenu) {
+        document.getElementsByTagName("nav")[0].style.backgroundColor = colorMenu;
+        const ajustesColor = document.getElementById('ajustesColor');
+        if (ajustesColor) {
+            ajustesColor.value = colorMenu;
+        }
+
+    }
+
+    // coge el modo día o noche del localStorage y lo aplica a la página. Si no existe coge por defecto el modo noche
     let modo = window.localStorage.getItem("--modo");
     if (!modo) {
         modo = "noche";
